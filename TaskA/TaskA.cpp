49e232cdb5b0e7fb0b34dad6bd75eb90538d0d56
stpetrakov <stpetrakov@yandex.ru>
2024-05-01 18:16:28 +0300

    fixed

--- TaskA/TaskA.cpp
+++ TaskA/TaskA.cpp
@@ -1,87 +1,188 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+#include <limits.h>
 
-struct my_stack
-{
-    int data[10000];
-    int Size;
+#define compression_coeff 4
+#define expansion_coeff 2
+
+enum result{
+    success = 1,
+    error = INT_MIN
 };
 
-void push (struct my_stack *Stack, int n) {
-    int x = Stack->Size;
-    Stack->data[x] = n;
-    Stack->Size++;
-    printf ("ok\n");
+struct Stack {
+    void* data;
+    size_t Size;
+    size_t Capacity;
+    size_t ElemSize;
+};
+
+struct Stack* stack_create () {
+
+    struct Stack* st = (struct Stack*) calloc(1, sizeof(struct Stack));
+    
+    assert (st != NULL);
+
+    st->Size = 0;
+    st->ElemSize = sizeof (int);
+    st->Capacity = 1;
+
+    st->data = (void*) calloc(st->Capacity, sizeof(int));
+
+    assert (st->data != NULL);
+
+    return st;
 }
 
-void pop (struct my_stack *Stack) {
-    if (Stack->Size == 0) {
-        printf ("error\n");
+int stack_push (struct Stack* st, void* buffer) {
+    if (buffer == NULL) 
+        return error;
+
+    if (st == NULL)
+        return error;
+
+    if (st->data == NULL)
+        return error;
+
+    if (st->Capacity == 0)
+       return error;
+
+    if (st->ElemSize == 0)
+        return error;
+    
+    if (st->Capacity == st->Size) {
+        st->Capacity *= expansion_coeff;
+        st->data = (void*) realloc (st->data, st->Capacity*st->ElemSize);
+
+        assert (st->data != NULL);
     }
-    else {
-        Stack->Size--;
-        int x = Stack->Size;
-        printf("%d\n", Stack->data[x]);
+
+    memcpy ((char*) st->data + st->Size * st->ElemSize, buffer, st->ElemSize);
+    st->Size++;
+
+    return success;
+}
+
+int stack_top (struct Stack* st, void* buffer) {
+    if (st == NULL)
+        return error;
+
+    if (st->data == NULL)
+        return error;
+
+    if (st->Capacity == 0)
+       return error;
+
+    if (st->ElemSize == 0)
+        return error;
+
+    memcpy (buffer, (char*) st->data + (st->Size-1) * st->ElemSize, st->ElemSize);
+
+    return (success);
+}
+
+int stack_pop(struct Stack* st) {
+    if (st == NULL)
+        return INT_MIN; 
+
+    if (st->data == NULL)
+        return INT_MIN; 
+
+    if (st->Capacity == 0)
+        return INT_MIN; 
+
+    if (st->ElemSize == 0)
+        return INT_MIN; 
+
+    if (st->Size == 0)
+        return INT_MIN;  
+
+    st->Size--;
+
+    int popped_element;
+    memcpy(&popped_element, (char*)st->data + (st->Size * st->ElemSize), st->ElemSize);
+
+    if (st->Capacity / expansion_coeff >= st->Size) {
+        st->Capacity /= expansion_coeff;
+        st->data = realloc(st->data, st->Capacity * st->ElemSize);
+
+        if (st->data == NULL)
+            return INT_MIN;  
     }
+
+    return popped_element;  
 }
 
-void back (struct my_stack *Stack) {
-    int x = Stack->Size;
-    if (Stack->Size == 0) {
-        printf ("error\n");
-    }
-    else
-        printf("%d\n", Stack->data[x-1]);
+
+struct Stack* stack_del (struct Stack* st) {
+    assert (st != NULL);
+    assert (st->data != NULL);
+    assert (st->Capacity != 0);
+    assert (st->ElemSize != 0);
+
+    free (st->data);
+    free (st);
+
+    return NULL;
 }
 
-void Size (struct my_stack *Stack) {
-    int x = Stack->Size;
-    printf ("%d\n", x);
-}
+int stack_clear (struct Stack* st) {
+    assert (st != NULL);
 
-void clear (struct my_stack *Stack) {
-    Stack->Size = 0;
-    printf ("ok\n");
-}
-
-void Exit (struct my_stack *Stack) {
-    printf ("bye\n");
+    st->Size = 0;
+    return success;
 }
 
 int main() {
-    struct my_stack Stack;
-    char a[10];
-    Stack.Size = 0;
-    while (1>0) {
-        scanf("%s", a);
+    struct Stack *stack = stack_create();
+    char command[10];
 
-        if (strcmp(a, "push") == 0) {
+    while (1) {
+        scanf ("%s", command);
+
+        if (strcmp(command, "push") == 0) {
             int n;
-            scanf("%d", &n);
-            push (&Stack, n);
+            scanf ("%d", &n);
+            if (stack_push (stack, &n) == success)
+                printf ("ok\n");
         }
-        else if (strcmp(a, "pop") == 0)
-        {
-            pop (&Stack);
+
+        else if (strcmp (command, "pop") == 0) {
+            int res = stack_pop (stack);
+            if (res == error) 
+                printf ("error\n");
+
+            else
+                printf ("%d\n", res);
         }
-        else if (strcmp(a, "back") == 0)
-        {
-            back (&Stack);
+
+        else if (strcmp(command, "back") == 0) {
+            int top_element;
+            stack_top (stack, &top_element);
+            if (top_element == error)
+                printf ("error\n");
+
+            else
+                printf ("%d\n", top_element);  
         }
-        else if (strcmp(a, "size") == 0)
-        {
-            Size (&Stack);
+
+        else if (strcmp(command, "size") == 0) {
+            printf("%ld\n", stack->Size);
         }
-        else if (strcmp(a, "clear") == 0)
-        {
-            clear (&Stack);
+
+        else if (strcmp(command, "clear") == 0) {
+            if (stack_clear (stack) == success)
+                printf ("ok\n");
         }
-        else
-        {
-            Exit (&Stack);
+
+        else {
+            stack_del (stack);
+            printf ("bye\n");
             break;
         }
     }
+
     return 0;
 }
