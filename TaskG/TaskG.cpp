e04d45639e339db7af9381a418fa15834bcf0fef
stpetrakov <stpetrakov@yandex.ru>
2024-05-13 14:15:32 +0300

    fixed

--- TaskG/TaskG.cpp
+++ TaskG/TaskG.cpp
@@ -4,25 +4,33 @@
 #include <string.h>
 #include <assert.h>
 
+#define INF 1000000000
+
 struct Node {
-    int x;
-    double y;
+    int key;
+    double priority;
     int size;
     struct Node* left;
     struct Node* right;
 };
 
-struct Node* createNode(int x) {
+struct Node* createNode(int key) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     assert(newNode != NULL);
 
-    newNode->x = x;
-    newNode->y = rand() / (RAND_MAX + 1.0);
+    newNode->key = key;
+    newNode->priority = rand() / (RAND_MAX + 1.0);
     newNode->size = 1;
     newNode->left = NULL;
     newNode->right = NULL;
 
     return newNode;
+}
+
+void swap(struct Node *a, struct Node *b) {
+    struct Node temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int size(struct Node* node) {
@@ -40,20 +48,17 @@ struct Node* merge(struct Node* left, struct Node* right) {
         return left ? left : right;
     }
 
-    if (left->y > right->y) {
-        left->right = merge(left->right, right);
-        updateSize(left);
-        return left;
-    } 
-    
-    else {
-        right->left = merge(left, right->left);
-        updateSize(right);
-        return right;
+    if (left->priority > right->priority) {
+        swap(left, right);
     }
+
+    left->right = merge(left->right, right);
+    updateSize(left);
+    return left;
+
 }
 
-void split(struct Node* node, int x, struct Node** left, struct Node** right) {
+void split(struct Node* node, int key, struct Node** left, struct Node** right) {
     assert(left != NULL);
     assert(right != NULL);
 
@@ -61,37 +66,49 @@ void split(struct Node* node, int x, struct Node** left, struct Node** right) {
         *left = *right = NULL;
     } 
     
-    else if (node->x <= x) {
+    else if (node->key <= key) {
         *left = node;
-        split(node->right, x, &((*left)->right), right);
+        split(node->right, key, &((*left)->right), right);
         updateSize(*left);
     } 
     
     else {
         *right = node;
-        split(node->left, x, left, &((*right)->left));
+        split(node->left, key, left, &((*right)->left));
         updateSize(*right);
     }
 }
 
-struct Node* insert(struct Node* root, int x) {
+struct Node* insert(struct Node* root, int key) {
     struct Node *left, *right;
     assert(root != NULL);
 
-    split(root, x, &left, &right);
+    split(root, key, &left, &right);
 
-    if (!right || right->x != x) {
-        struct Node* newNode = createNode(x);
-        return merge(merge(left, newNode), right);
+    if (!right || right->key != key) {
+        struct Node* newNode = createNode(key);
+        
+        if (!right || key < right->key) {
+            newNode->right = right;
+            newNode->left = left;
+        } else {
+            newNode->left = left;
+            newNode->right = right->right;
+        }
+        
+        updateSize(newNode);
+        
+        return newNode;
     }
 
     return root;
 }
 
+
 struct Node* erase(struct Node* root, int key) {
     assert(root != NULL);
 
-    if (root->x == key) {
+    if (root->key == key) {
         root = merge(root->left, root->right);
 
         if (root != NULL) {
@@ -99,7 +116,7 @@ struct Node* erase(struct Node* root, int key) {
         }
     } 
     
-    else if (root->x < key) {
+    else if (root->key < key) {
         root->right = erase(root->right, key);
         
         if (root->right != NULL) {
@@ -115,19 +132,19 @@ struct Node* erase(struct Node* root, int key) {
         }
     }
 
-    updateSize (root);
+    updateSize(root);
     return root;
 }
 
-bool exists(struct Node* root, int x) {
+bool exists(struct Node* root, int key) {
     assert(root != NULL);
 
     while (root) {
-        if (root->x == x) {
+        if (root->key == key) {
             return true;
         } 
         
-        else if (root->x < x) {
+        else if (root->key < key) {
             root = root->right;
         } 
         
@@ -138,11 +155,11 @@ bool exists(struct Node* root, int x) {
     return false;
 }
 
-int next_val(struct Node* root, int x) {
-    int res = -1000000000;
+int next_value(struct Node* root, int key) {
+    int res = -INF;
     while (root != NULL) {
-        if (root->x > x) {
-            res = root->x;
+        if (root->key > key) {
+            res = root->key;
             root = root->left;
         } 
         
@@ -153,11 +170,11 @@ int next_val(struct Node* root, int x) {
     return res;
 }
 
-int prev_val(struct Node* root, int x) {
-    int res = -1000000000;
+int prev_value(struct Node* root, int key) {
+    int res = -INF;
     while (root) {
-        if (root->x < x) {
-            res = root->x;
+        if (root->key < key) {
+            res = root->key;
             root = root->right;
         } 
         
@@ -168,16 +185,16 @@ int prev_val(struct Node* root, int x) {
     return res;
 }
 
-int kth(struct Node* root, int k) {
+int find_kth_order_statistic(struct Node* root, int k) {
     if (root == NULL || k >= size(root) || k < 0) {
-        return -1000000000; 
+        return -INF; 
     }
 
     while (root != NULL) {
         int leftSize = size(root->left);
 
         if (k == leftSize) {
-            return root->x;
+            return root->key;
         } 
         
         else if (k < leftSize) {
@@ -189,24 +206,28 @@ int kth(struct Node* root, int k) {
             root = root->right;
         }
     }
-    return -1000000000; 
+    return -INF; 
 }
 
 
-struct Node* del(struct Node* root, int x) {
+struct Node* del(struct Node* root, int key) {
     if (!root) return NULL;
+
+    if (root->size <= 0)
+        return root;
+
     root->size--;
 
-    if (root->x == x) {
+    if (root->key == key) {
         return merge (root->left, root->right);
     }
 
-    if (x < root->x) {
-        root->left = del (root->left, x);
+    if (key < root->key) {
+        root->left = del (root->left, key);
     } 
     
     else {    
-        root->right = del (root->right, x);
+        root->right = del (root->right, key);
     }
 
     return root;
@@ -234,8 +255,8 @@ int main() {
         else if (strcmp (operation, "next") == 0) {
             assert (scanf("%d", &value) == 1);
 
-            if (next_val (root, value) != -1000000000)
-                printf ("%d\n", next_val (root, value));
+            if (next_value (root, value) != -INF)
+                printf ("%d\n", next_value (root, value));
 
             else
                 printf ("none\n");
@@ -243,8 +264,8 @@ int main() {
         
         else if (strcmp (operation, "prev") == 0) {
             assert (scanf("%d", &value) == 1);
-            if (prev_val (root, value) != -1000000000)
-                printf ("%d\n", prev_val (root, value));
+            if (prev_value (root, value) != -INF)
+                printf ("%d\n", prev_value (root, value));
 
             else
                 printf ("none\n");
@@ -257,8 +278,8 @@ int main() {
         
         else if (strcmp(operation, "kth") == 0) {
             assert (scanf("%d", &value) == 1);
-            if (kth (root, value) != -1000000000)
-                printf ("%d\n", kth(root, value));
+            if (find_kth_order_statistic (root, value) != -INF)
+                printf ("%d\n", find_kth_order_statistic (root, value));
 
             else
                 printf ("none\n");
