4b91f38b655f8bf1ccaf8a628a682df3861cada5
stpetrakov <stpetrakov@yandex.ru>
2024-05-15 18:47:41 +0300

    fixed??

--- TaskD/TaskD.cpp
+++ TaskD/TaskD.cpp
@@ -1,44 +1,53 @@
-#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
 
-#define mod 2147483647LL
-#define FIRST_LEVEL_SIZE  1000007
-#define SECOND_LEVEL_SIZE 19
+#define MOD1 1000007
+#define MAX_M2 100
 
 struct ListNode {
     unsigned int key;
     struct ListNode* next;
 };
 
+
 struct FirstLevelNode {
-    struct ListNode* table[SECOND_LEVEL_SIZE];
+    struct ListNode* table[MAX_M2];
 };
+
 
 struct PerfectHashTable {
-    struct FirstLevelNode* table[FIRST_LEVEL_SIZE];
+    struct FirstLevelNode* table[MOD1];
 };
 
-unsigned int firstLevelHash(unsigned int key) {
-    return key % FIRST_LEVEL_SIZE;
+
+unsigned int random(unsigned int a, unsigned int b) {
+    return rand() % (b - a + 1) + a;
 }
 
-unsigned int secondLevelHash(unsigned int key) {
-    return key % SECOND_LEVEL_SIZE;
+
+unsigned int firstLevelHash(unsigned int key) {
+    return key % MOD1;
 }
+
+
+unsigned int secondLevelHash(unsigned int key, unsigned int m2) {
+    return key % m2;
+}
+
 
 void insert(struct PerfectHashTable* pht, unsigned int key) {
-    assert (pht);
+    assert(pht);
 
     unsigned int firstIndex = firstLevelHash(key);
-    unsigned int secondIndex = secondLevelHash(key);
+    unsigned int secondIndex = secondLevelHash(key, MAX_M2);
 
     if (pht->table[firstIndex] == NULL) {
         pht->table[firstIndex] = (struct FirstLevelNode*)malloc(sizeof(struct FirstLevelNode));
-        assert (pht->table);
+        assert(pht->table[firstIndex]);
         
-        for (unsigned int i = 0; i < SECOND_LEVEL_SIZE; i++) {
+        for (unsigned int i = 0; i < MAX_M2; i++) {
             pht->table[firstIndex]->table[i] = NULL;
         }
     }
@@ -51,11 +60,10 @@ void insert(struct PerfectHashTable* pht, unsigned int key) {
 }
 
 bool search(struct PerfectHashTable* pht, unsigned int key) {
-    assert (pht);
-    assert (pht->table);
+    assert(pht);
 
     unsigned int firstIndex = firstLevelHash(key);
-    unsigned int secondIndex = secondLevelHash(key);
+    unsigned int secondIndex = secondLevelHash(key, MAX_M2);
 
     if (pht->table[firstIndex] != NULL && pht->table[firstIndex]->table[secondIndex] != NULL)
         return true; 
@@ -64,12 +72,11 @@ bool search(struct PerfectHashTable* pht, unsigned int key) {
 }
 
 void freeHashTable(struct PerfectHashTable* pht) {
-    assert (pht);
-    assert (pht->table);
+    assert(pht);
 
-    for (unsigned int i = 0; i < FIRST_LEVEL_SIZE; i++) {
+    for (unsigned int i = 0; i < MOD1; i++) {
         if (pht->table[i] != NULL) {
-            for (unsigned int j = 0; j < SECOND_LEVEL_SIZE; j++) {
+            for (unsigned int j = 0; j < MAX_M2; j++) {
                 struct ListNode* current = pht->table[i]->table[j];
             
                 while (current != NULL) {
@@ -78,39 +85,83 @@ void freeHashTable(struct PerfectHashTable* pht) {
                     free(temp);
                 }
             }
+
             free(pht->table[i]);
         }
     }
 }
 
+bool checkCollisions(struct PerfectHashTable* pht, unsigned int n) {
+    unsigned int collisionsSum = 0;
+
+    for (unsigned int i = 0; i < MOD1; i++) {
+        if (pht->table[i] != NULL) {
+            unsigned int collisionCount = 0;
+
+            for (unsigned int j = 0; j < MAX_M2; j++) {
+                struct ListNode* current = pht->table[i]->table[j];
+
+                if (current != NULL) {
+                    collisionCount++;
+                }
+            }
+
+            collisionsSum += collisionCount * collisionCount;
+        }
+    }
+    return collisionsSum <= 4 * n;
+}
+
 int main() {
     struct PerfectHashTable pht;
-
-    for (unsigned int i = 0; i < FIRST_LEVEL_SIZE; i++) {
+    for (unsigned int i = 0; i < MOD1; i++) {
         pht.table[i] = NULL;
     }
 
+    unsigned int hash1 = random(1, MOD1 - 1);
+    unsigned int hash2 = random(1, MOD1 - 1);
+
+    while (true) {
+        if (checkCollisions(&pht, MOD1)) {
+            break;
+        }
+
+        hash1 = random(1, MOD1 - 1);
+        hash2 = random(1, MOD1 - 1);
+    }
+
     unsigned int n;
-    assert (scanf("%u", &n) == 1);
+
+    if (scanf("%u", &n) != 1) {
+        return 0; 
+    }
 
     for (unsigned int i = 0; i < n; i++) {
         unsigned int x;
-        assert (scanf("%u", &x) == 1);
-        insert (&pht, x + mod);
-    }
-
-    while (true) {
-        unsigned int x = 0;
 
         if (scanf("%u", &x) != 1) {
             break; 
         }
-        
-        if (search(&pht, x + mod))
+
+        unsigned int hash = (hash1 * x + hash2) % MOD1;
+        insert(&pht, hash);
+    }
+
+    while (true) {
+        unsigned int x;
+
+        if (scanf("%u", &x) != 1) {
+            break; 
+        }
+
+        unsigned int hash = (hash1 * x + hash2) % MOD1;
+        if (search(&pht, hash)) {
             printf("YES\n");
+        } 
         
-        else
+        else {
             printf("NO\n");
+        }
     }
 
     freeHashTable(&pht);
